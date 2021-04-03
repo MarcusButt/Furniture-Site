3d98e4c7ac932aee4f3964604f9453e5b7febbe0
MarcusButt <MarcusButt@users.noreply.github.com>
2021-04-03 11:44:17 -0230

    Tried to Fix PHP Script (it didn't work)

--- contactform.php
+++ contactform.php
@@ -1,15 +1,16 @@
 <?php
 
-if(isset($_GET['submit'])) {
-    $name = $_GET['name'];
-    $title = $_GET['title'];
-    $comment = $_GET['comment'];
-    $email = $_GET['email'];
+if($_POST['submit']) {
+    $name = $_POST['name'];
+    $title = $_POST['title'];
+    $comment = $_POST['comment'];
+    $email = $_POST['email'];
 
     $mailTo = "mjcb32@mun.ca";
     $headers = "From: ".$email;
     $txt = "You have received an email from ".$name.".\n\n".$message;
 
     mail($mailTo, $txt, $headers);
-    header("Location: index.htm?mailsend")
 }
+
+?>
