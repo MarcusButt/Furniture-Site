ad8a0d39a8482bb70c28a1c4f673fabe04469f6d
MarcusButt <MarcusButt@users.noreply.github.com>
2021-04-07 16:30:38 -0230

    Added "Go To Top" Button

---
+++ actions.js
@@ -0,0 +1,17 @@
+
+mybutton = document.getElementById("TopButton");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
